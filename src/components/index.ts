33910a62da877f38cba0fcbe728a1c021dba4ec7
Andrei <kuzmenko_andrey_@mail.ru>
2024-03-13 17:47:42 +0300

    Sprint 1 (#4)
    * Создан драфт мессенджера

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Form } from "./form";
+export { Input } from "./input";
+export { Button } from "./button";
+export { Warning } from "./warning";
+export { Avatar } from "./avatar";
+export { PageWrapper } from "./page-wrapper";
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export * from './error';
+export * from './feed';
+export * from './login';
+export * from './not-found';
+export * from './profile';
+export * from './register';
