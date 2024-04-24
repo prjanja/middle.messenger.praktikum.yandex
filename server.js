89cc884b12f1dd74a68c451123cdc9ed71c58679
Андрей <kuzmenko_andrey_@mail.ru>
2024-04-25 00:00:22 +0300

    исправлен конфиг экспресса

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
+const __dirname = path.resolve();
 
 app.use(express.static('./dist'));
 
 app.listen(PORT);
+
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
